aff581b9e29b9f3bc84b4bbb9f349daa6608b0fe
Zenaku54545 <cai.l.i.n.5.us.m@gmail.com>
2025-03-07 17:01:56 +0630

    Update play.py

--- maythusharmusic/utils/inline/play.py
+++ maythusharmusic/utils/inline/play.py
@@ -27,7 +27,7 @@ def track_markup(_, videoid, user_id, channel, fplay):
     return buttons
 
 
-def stream_markup_timer(_, chat_id, played, dur):
+def stream_markup_timer(_, vidid, chat_id, played, dur):
     played_sec = time_to_seconds(played)
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
@@ -79,7 +79,7 @@ def stream_markup_timer(_, chat_id, played, dur):
     return buttons
 
 
-def stream_markup(_, chat_id):
+def stream_markup(_, videoid, chat_id):
     buttons = [
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
