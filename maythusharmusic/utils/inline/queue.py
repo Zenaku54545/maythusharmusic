633be4ef5636a88b8c7959f03c984fcccc91965a
Zenaku54545 <cai.l.i.n.5.us.m@gmail.com>
2025-03-15 23:45:22 +0630

    Update queue.py

--- maythusharmusic/utils/inline/queue.py
+++ maythusharmusic/utils/inline/queue.py
@@ -75,3 +75,37 @@ def aq_markup(_, chat_id):
     return buttons
 
 
+def queuemarkup(_, chat_id):
+
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_5"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="ᴘᴀᴜsᴇ",
+                callback_data=f"ADMIN Pause|{chat_id}",
+            ),
+            InlineKeyboardButton(text="sᴛᴏᴘ", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="sᴋɪᴘ", callback_data=f"ADMIN Skip|{chat_id}"),
+        ],
+        [
+            InlineKeyboardButton(
+                text="ʀᴇsᴜᴍ", callback_data=f"ADMIN Resume|{chat_id}"
+            ),
+            InlineKeyboardButton(
+                text="ʀᴇᴘʟᴀ", callback_data=f"ADMIN Replay|{chat_id}"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="๏ ᴍᴏʀᴇ ๏",
+                url="https://t.me/sasukemusicbotsupportchat",
+            ),
+        ],
+    ]
+
+    return buttons
