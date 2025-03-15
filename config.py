42470f22146f6f9afb1d555895078fc9f85d15ed
Zenaku54545 <cai.l.i.n.5.us.m@gmail.com>
2025-03-15 22:27:38 +0630

    Update config.py

--- config.py
+++ config.py
@@ -68,7 +68,7 @@ SONG_DOWNLOAD_DURATION_LIMIT = int(getenv("SONG_DOWNLOAD_DURATION_LIMIT", "2000"
 
 # Telegram audio and video file size limit (in bytes)
 TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
-TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
+TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 3145728000))
 # Checkout https://www.gbmb.org/mb-to-bytes for converting mb to bytes
 
 
