176232438c689866f580db568db8262eb9f61a85
Zenaku54545 <cai.l.i.n.5.us.m@gmail.com>
2025-03-18 20:27:54 +0630

    Update a_welcome.py

--- maythusharmusic/plugins/Queenmedusa/a_welcome.py
+++ maythusharmusic/plugins/Queenmedusa/a_welcome.py
@@ -12,7 +12,7 @@ async def welcome(_, event: ChatMemberUpdated):
     if event.new_chat_member and event.new_chat_member.status == "member":
         user = event.new_chat_member.user
         chat_id = event.chat.id
-        await app.send_message(chat_id, f"{user.first_name} ωᴇℓᴄᴏᴍᴇ ʙᴀʙʏ 🎉")
+        await app.send_message(chat_id, f"{user.mention} ωᴇℓᴄᴏᴍᴇ ʙᴀʙʏ 🎉")
 
 # Start the bot
 if __name__ == "__main__":
